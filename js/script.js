4c886502a52c7078239a6a0d862c36dd966b30d7
Anna <67719118+brasspetals@users.noreply.github.com>
2021-02-03 14:23:40 -0600

    Combine mobile and desktop navs, correct border width on bordered links, unset disable scroll for desktop menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const btn = document.querySelector(".menu-btn");
 const btnIcon = document.querySelector(".menu-btn__icon");
-const nav = document.querySelector(".nav-mobile");
+const nav = document.querySelector(".nav");
 
 function toggleMenu() {
   // Mobile navigation menu toggle
-  nav.classList.toggle("nav-mobile--hidden");
+  nav.classList.toggle("nav--hidden");
   document.body.classList.toggle("disable-scroll");
 
   // Switch menu button icon on nav toggle
-  if (nav.classList.contains("nav-mobile--hidden")) {
+  if (nav.classList.contains("nav--hidden")) {
     btnIcon.src = "./images/icon-hamburger.svg";
   } else {
     btnIcon.src = "./images/icon-close.svg";
