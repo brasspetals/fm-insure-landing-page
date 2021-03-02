e09c010e01a9895b8719a33576008382a84e475f
Anna <67719118+brasspetals@users.noreply.github.com>
2021-03-02 14:23:35 -0600

    Move menu-btn inside nav element and add aria attributes for better accessibility
    - also added aria-label to footer nav
    - switched original header nav element to div and rename

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".menu-btn");
 const btnIcon = document.querySelector(".menu-btn__icon");
-const nav = document.querySelector(".nav");
+const nav = document.querySelector(".nav-menu-wrapper");
 
 function toggleMenu() {
   // Mobile navigation menu toggle
@@ -9,9 +9,11 @@ function toggleMenu() {
 
   // Switch menu button icon on nav toggle
   if (nav.classList.contains("nav--hidden")) {
+    btn.setAttribute("aria-expanded", "false");
     btnIcon.src = "./images/icon-hamburger.svg";
   } else {
     btnIcon.src = "./images/icon-close.svg";
+    btn.setAttribute("aria-expanded", "true");
   }
 }
 
